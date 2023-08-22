42c71fe849601de8f7bd5efa84f1c8f0b2c1f104
Chris Arter <chris@arter.dev>
2023-08-22 11:40:36 -0400

    convert stubs to ES modules

--- src/stubs/cypress.config.js
+++ src/stubs/cypress.config.js
@@ -1,6 +1,6 @@
-const { defineConfig } = require('cypress')
+import { defineConfig } from 'cypress';
 
-module.exports = defineConfig({
+export default defineConfig({
     chromeWebSecurity: false,
     retries: 2,
     defaultCommandTimeout: 5000,
